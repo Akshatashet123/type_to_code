ea864219f88a214e9b77cd536ec227356c9db128
Akshatashet123 <69043784+Akshatashet123@users.noreply.github.com>
2020-08-16 15:42:32 +0530

    assesment

--- isogram_ex.cpp
+++ isogram_ex.cpp
@@ -3,24 +3,37 @@
 #include<ctype.h>
 
 int main(){
-	char str[10];
-	int i,len,flag=0;
-	;
+	char str[10],temp;
+	int i,len,flag=0,j;
+	
 	scanf("%s",str);
 	len=strlen(str);
 	for(i=0;i<=len;i++){
 
 	str[i]=tolower(str[i]);
 }
-for(i=0;i<=len;i++){
+for(i=0;i<len-1;i++){
+	for(j=i+1;j<len;j++){
+		if(str[i]>str[j]){
+			temp=str[i];
+			str[i]=str[j];
+			str[j]=temp;
+			
+		}
+	}
+}
+printf("%s\n",str);
 
-		while(str!='\0'){
+          for(i=0;i<=len;i++){
+
+	     	while(str!='\0'){
 			if(str[i]==str[i+1])
 			flag=1;
 			break;
 		}
-}
-	if(flag==0)
+	}
+
+	if(flag==1)
 	printf("false");
 	else
 	printf("true");
